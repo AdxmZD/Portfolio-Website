e92ad8fb08272aea941a9d8f3c75526ecb602967
Adam Deen <adamdeen@hotmail.co.uk>
2023-03-10 00:05:08 +0000

    configured navbar component and css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { useState } from "react";
-import useMediaQuery from "./hooks/useMediaQuery"
+import React, { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home');
+  const [currentPage, setCurrentPage] = useState("home");
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
+      <Navbar selectedPage={currentPage} setSelectedPage={setCurrentPage} />
     </div>
   );
 }
